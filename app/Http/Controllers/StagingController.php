065c874016b796df5011d146cfe92296348c95ab
Tomoya Koyanagi <tomk79@gmail.com>
2019-06-10 11:05:55 +0900

    <plum> いま埋められるだけの変数を埋めた

--- app/Http/Controllers/StagingController.php
+++ app/Http/Controllers/StagingController.php
@@ -28,27 +28,8 @@ class StagingController extends Controller
 	 */
 	public function index(Request $request, Project $project, $branch_name){
 
-		$page_param = $request->page_path;
-		$page_id = $request->page_id;
-
-		$project_name = $project->project_name;
-		$project_path = get_project_workingtree_dir($project_name, $branch_name);
-
-		$path_current_dir = realpath('.'); // 元のカレントディレクトリを記憶
-		chdir($project_path);
-		$data_json = shell_exec('php .px_execute.php /?PX=px2dthelper.get.all\&filter=false\&path='.$page_id);
-		$current = json_decode($data_json);
-		chdir($path_current_dir); // 元いたディレクトリへ戻る
-
-		$sitemap_files = \File::files($current->realpath_homedir.'sitemaps/');
-		foreach($sitemap_files as $file) {
-			if($file->getExtension() === 'xlsx') {
-				$get_files[] = $file;
-			} else {
-				$destroy_files[] = $file;
-			}
-		}
-
+        $realpath_pj_git_root = get_project_workingtree_dir($project->project_name, $branch_name);
+		$parsed_git_url = parse_url( $project->git_url );
 
 		$plum = new \hk\plum\main(
 			array(
@@ -73,9 +54,14 @@ class StagingController extends Controller
 					//
 					array(
 						'name' => 'preview1',
-						'path' => './../repos/preview1/',
-						'url' => 'http://preview1.localhost/'
-					)
+						'path' => './../bd_data/projects/'.urlencode($project->project_name).'/stagings/preview1/',
+						'url' => 'https://preview1.'.urlencode($project->id).'.burdock.localhost/'
+					),
+					array(
+						'name' => 'preview2',
+						'path' => './../bd_data/projects/'.urlencode($project->project_name).'/stagings/preview2/',
+						'url' => 'https://preview2.'.urlencode($project->id).'.burdock.localhost/'
+					),
 				),
 
 				// Git情報定義
@@ -83,27 +69,27 @@ class StagingController extends Controller
 					
 					// リポジトリのパス
 					// ウェブプロジェクトのリポジトリパスを設定。
-					'repository' => './../repos/master/',
+					'repository' => $realpath_pj_git_root,
 
 					// プロトコル
 					// ※現在はhttpsのみ対応
-					'protocol' => 'https',
+					'protocol' => $parsed_git_url['scheme'],
 
 					// ホスト
 					// Gitリポジトリのhostを設定。
-					'host' => 'host.com',
+					'host' => $parsed_git_url['host'],
 
 					// GitリポジトリのURL
 					// Gitリポジトリのhost以下のパスを設定。
-					'url' => 'host.com/path/to.git',
+					'url' => $parsed_git_url['path'],
 
 					// ユーザ名
 					// Gitリポジトリのユーザ名を設定。
-					'username' => 'user',
+					'username' => \Crypt::decryptString( $project->git_username ),
 
 					// パスワード
 					// Gitリポジトリのパスワードを設定。
-					'password' => 'pass'
+					'password' => \Crypt::decryptString( $project->git_password ),
 				)
 			)
 		);
@@ -116,7 +102,6 @@ class StagingController extends Controller
 			[
 				'project' => $project,
 				'branch_name' => $branch_name,
-				'page_param' => $page_param,
 				'plum_std_out' => $plum_std_out,
 			],
 			compact('current', 'get_files')
