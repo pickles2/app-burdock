a033e7fb1a0996e782d03f3bc42c6481127915ed
Tomoya Koyanagi <tomk79@gmail.com>
2019-06-29 16:35:45 +0900

    git status, pull, commit, push の機能を追加

--- app/Http/Controllers/GitController.php
+++ app/Http/Controllers/GitController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Project;
-use App\Http\Requests\StoreSitemap;
 
 class GitController extends Controller
 {
@@ -27,8 +26,11 @@ class GitController extends Controller
 	 * @return \Illuminate\Http\Response
 	 */
 	public function index(Request $request, Project $project, $branch_name){
+		if( !strlen($branch_name) ){
+			$branch_name = \get_git_remote_default_branch_name($project->git_url);
+		}
 
-        $realpath_pj_git_root = './../bd_data/projects/'.urlencode($project->project_code).'/stagings/master/';
+		$realpath_pj_git_root = \get_project_workingtree_dir($project->project_code, $branch_name);
 
 		return view(
 			'git.index',
@@ -38,4 +40,60 @@ class GitController extends Controller
 			]
 		);
 	}
+
+	/**
+	 * git-status
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function gitStatus(Request $request, Project $project, $branch_name){
+		$git = new \pickles2\burdock\git($project->id, $branch_name);
+		$rtn = array();
+		array_push($rtn, $git->git('status'));
+		header('Content-type: application/json');
+		return json_encode($rtn);
+	}
+
+	/**
+	 * git-pull
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function gitPull(Request $request, Project $project, $branch_name){
+		$git = new \pickles2\burdock\git($project->id, $branch_name);
+		$rtn = array();
+		array_push($rtn, $git->git('pull origin'));
+		header('Content-type: application/json');
+		return json_encode($rtn);
+	}
+
+	/**
+	 * git-commit
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function gitCommit(Request $request, Project $project, $branch_name){
+		$git = new \pickles2\burdock\git($project->id, $branch_name);
+		$rtn = array();
+		array_push($rtn, $git->git('add ./'));
+		$cmd_commit = 'commit -m "test commit"';
+		$cmd_commit = 'commit -m '.escapeshellarg($request->commit_message);
+		array_push($rtn, $git->git($cmd_commit));
+		header('Content-type: application/json');
+		return json_encode($rtn);
+	}
+
+	/**
+	 * git-push
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function gitPush(Request $request, Project $project, $branch_name){
+		$git = new \pickles2\burdock\git($project->id, $branch_name);
+		$rtn = array();
+		array_push($rtn, $git->git('push origin'));
+		header('Content-type: application/json');
+		return json_encode($rtn);
+	}
+
 }
--- resources/views/git/index.blade.php
+++ resources/views/git/index.blade.php
@@ -6,8 +6,78 @@
 @section('content')
 <div class="container">
 	<h1>Git</h1>
-	<div class="contents">
-		<p>開発中の機能です。</p>
+	<div class="contents cont-git">
+		<div class="btn-group" role="group">
+			<button class="btn px2-btn cont-btn-git-status">status</button>
+			<button class="btn px2-btn cont-btn-git-pull">pull</button>
+			<button class="btn px2-btn cont-btn-git-commit">commit</button>
+			<button class="btn px2-btn cont-btn-git-push">push</button>
+		</div>
+		<pre><code></code></pre>
 	</div>
 </div>
+@endsection
+
+@section('script')
+<script>
+var $preview = document.querySelector('.cont-git pre code');
+
+function gitCmd(command_name, params){
+	var _this = this;
+	var btns = document.querySelectorAll('.cont-git button.px2-btn');
+	btns.forEach(function(btn){
+		btn.setAttribute('disabled', 'disabled');
+	});
+	$preview.innerHTML = '';
+	var method = 'post';
+	switch(command_name){
+		case 'status':
+			method = 'get';
+			break;
+	}
+	$.ajax({
+		type : method,
+		url : "/git/{{ $project->project_code }}/{{ $branch_name }}/git-"+command_name,
+		headers: {
+			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		},
+		contentType: 'application/json',
+		dataType: 'json',
+		data: JSON.stringify(params || {}),
+		success: function(data){
+			// console.log(data);
+			for( var idx in data ){
+				$preview.innerHTML += data[idx].stdout;
+				$preview.innerHTML += data[idx].stderr;
+			}
+		},
+		complete: function(){
+			btns.forEach(function(btn){
+				btn.removeAttribute('disabled');
+			});
+		}
+	});
+}
+
+window.addEventListener('load', function(e){
+	document.querySelector('.cont-btn-git-status').addEventListener('click', function(){
+		gitCmd('status');
+	});
+	document.querySelector('.cont-btn-git-pull').addEventListener('click', function(){
+		gitCmd('pull');
+	});
+	document.querySelector('.cont-btn-git-commit').addEventListener('click', function(){
+		var commitMsg = prompt('commit message?');
+		if(!commitMsg){
+			return;
+		}
+		gitCmd('commit', {
+			'commit_message': commitMsg
+		});
+	});
+	document.querySelector('.cont-btn-git-push').addEventListener('click', function(){
+		gitCmd('push');
+	});
+});
+</script>
 @endsection
