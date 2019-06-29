fe7ecc329be221fd05d90f9a414658bcb92b4162
Tomoya Koyanagi <tomk79@gmail.com>
2019-06-29 17:05:02 +0900

    <Theme> テーマ編集画面の入れ物だけ追加

---
+++ resources/views/theme/index.blade.php
@@ -0,0 +1,13 @@
+@php
+	$title = __('Theme');
+@endphp
+@extends('layouts.px2_project')
+
+@section('content')
+<div class="container">
+	<h1>Theme</h1>
+	<div class="contents">
+		<p>開発中の機能です。</p>
+	</div>
+</div>
+@endsection
