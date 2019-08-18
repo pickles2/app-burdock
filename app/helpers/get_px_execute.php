1be1307e14e2b6bdf2de67b8146bc92d021c8ad2
Tomoya Koyanagi <tomk79@gmail.com>
2019-08-18 13:12:42 +0900

    entryScriptのパスが読み込めない事がある不具合を修正

--- app/helpers/get_px_execute.php
+++ app/helpers/get_px_execute.php
@@ -2,11 +2,11 @@
 
 function get_px_execute($project_code, $branch_name, $option)
 {
-	//
 	$project_path = get_project_workingtree_dir($project_code, $branch_name);
 	if( !is_dir($project_path) ){
 		return false;
 	}
+	$realpath_entry_script = $project_path.'/'.get_px_execute_path($project_code, $branch_name);
 	$path_current_dir = realpath('.'); // 元のカレントディレクトリを記憶
 
 	if(!\File::exists($project_path.'/composer.json')) {
@@ -15,12 +15,13 @@ function get_px_execute($project_code, $branch_name, $option)
 	if(!\File::exists($project_path.'/vendor')) {
 		return false;
 	}
-	if(!\File::exists(get_px_execute_path($project_code, $branch_name))) {
+
+	if(!\File::exists($realpath_entry_script)) {
 		return false;
 	}
 
 	chdir($project_path);
-	$bd_json = shell_exec('php '.get_px_execute_path($project_code, $branch_name).$option);
+	$bd_json = shell_exec('php '.$realpath_entry_script.' '.$option);
 	chdir($path_current_dir); // 元いたディレクトリへ戻る
 	$bd_object = json_decode($bd_json);
 
