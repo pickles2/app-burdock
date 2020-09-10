8e7be25c4002b4ee3ac1de551592f45aa1f5e359
Tomoya Koyanagi <tomk79@gmail.com>
2020-09-10 10:55:02 +0900

    `\URL::forceScheme('https')` を追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,30 +8,34 @@ use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-        // MySQL5.7.7、またはMariaDB10.2.2より古い場合に必要です。
-        Schema::defaultStringLength(191);
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
 
-        // グローバル変数
-        // 管理者のID番号を1とする
-        // 参照: https://stackoverflow.com/questions/28356193/
-        // config(['admin_id' => 1]);
-    }
+		// --------------------------------------
+		// MySQL5.7.7、またはMariaDB10.2.2より古い場合に必要です。
+		Schema::defaultStringLength(191);
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+		// --------------------------------------
+		// 強制的にHTTPS環境にします。
+		// `asset()` などに影響します。
+		// デフォルトは環境変数から自動的にセットされますが、
+		// ↓この行を書くことで強制的に HTTPS 環境として認識させます。
+		\URL::forceScheme('https');
+
+	}
+
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 }
