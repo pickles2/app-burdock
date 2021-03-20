e448b78fc67b85ba5e897dc41f3e46d0a23cc9c6
Tomoya Koyanagi <tomk79@gmail.com>
2021-03-21 03:47:27 +0900

    ブランチ名省略時にデフォルトの名前を探すようになった

--- app/Http/Middleware/project.php
+++ app/Http/Middleware/project.php
@@ -24,6 +24,7 @@ class project
 		$global->branch_name = null;
 		$global->px2all = null;
 		$global->cce = null;
+		$global->project = null;
 
 		$request_path = $request->path();
 
@@ -37,7 +38,21 @@ class project
 			$global->cce_id = null;
 			$global->project_code = $matched[2];
 			$global->branch_name = $matched[3];
+		}elseif( preg_match('/^([a-zA-Z0-9\_\-]+)\/([a-zA-Z0-9\_\-]+)(?:\/[\s\S]*)?$/', $request_path, $matched) ){
+			$global->function_name = $matched[1];
+			$global->cce_id = null;
+			$global->project_code = $matched[2];
 		}
+		if( strlen($global->project_code) ){
+			$global->project = \App\Project::where('project_code', $global->project_code)->first();
+		}
+		if( $global->project && !strlen($global->branch_name) ){
+			$global->branch_name = $global->project->git_main_branch_name;
+		}
+		if( !strlen($global->branch_name) ){
+			$global->branch_name = 'master';
+		}
+
 
 		if( strlen($global->project_code) && strlen($global->branch_name) ){
 			$global->px2all = px2query(
