3b1751108539faaa10c9dd212de9ff3eae5850c9
Tomoya Koyanagi <tomk79@gmail.com>
2021-04-01 13:03:31 +0900

    プロジェクトのセットアップ状態に応じてメニューの出し分けをするようになった。

--- resources/views/layouts/inc/header.blade.php
+++ resources/views/layouts/inc/header.blade.php
@@ -23,10 +23,12 @@
 					@else
 						@if( isset($project) && ! Request::is('*mypage*') && ! Request::is('/') && ! Request::is('setup/*'))
 							<li><a href="{{ url('home/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="home">ホーム</a></li>
+							@if( $global->project_status->isPxStandby )
 							<li><a href="{{ url('sitemaps/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="sitemaps">サイトマップ</a></li>
 							<li><a href="{{ url('themes/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="themes">テーマ</a></li>
 							<li><a href="{{ url('contents/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="contents">コンテンツ</a></li>
 							<li><a href="{{ url('publish/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="publish">パブリッシュ</a></li>
+							@endif
 						@endif
 
 						{{-- 認証関連のリンク --}}
@@ -78,26 +80,34 @@
 					</li>
 					@if( isset($project) && ! Request::is('*mypage*') && ! Request::is('/') && ! Request::is('setup/*'))
 						<li><a href="{{ url('projects/'.urlencode($project->project_code).'/edit') }}" data-name="projects">プロジェクト概要設定</a></li>
-						<li><a href="{{ url('composer/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="composer">Composerを操作する</a></li>
-						<li><a href="{{ url('git/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="git">Gitを操作する</a></li>
-						<li><a href="javascript:;">ツール</a>
-							<ul>
-								<li><a href="{{ url('search/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="search">検索</a></li>
-							</ul>
-						</li>
-						@if( isset($global->cce) && (is_object($global->cce) || is_array($global->cce)) )
-						<li><a href="javascript:;">拡張機能</a>
-							<ul>
-								@foreach($global->cce as $cce_id=>$cce_info)
-								<li><a href="{{ url('custom_console_extensions/'.urlencode($cce_id).'/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="custom_console_extensions.{{ $cce_id }}">{{ $cce_info->label }}</a></li>
-								@endforeach
-							</ul>
-						</li>
+						@if( $global->project_status->composerJsonExists )
+							<li><a href="{{ url('composer/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="composer">Composerを操作する</a></li>
 						@endif
-						<li><a href="{{ url('staging/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="staging">ステージング管理</a></li>
-						<li><a href="{{ url('delivery/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="delivery">配信管理</a></li>
-						<li><a href="{{ url('clearcache/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="clearcache">キャッシュを消去する</a></li>
-						<li><a href="{{ url('files-and-folders/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="files-and-folders">ファイルとフォルダ</a></li>
+						@if( $global->project_status->pathExists )
+							<li><a href="{{ url('git/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="git">Gitを操作する</a></li>
+						@endif
+						@if( $global->project_status->isPxStandby )
+							<li><a href="javascript:;">ツール</a>
+								<ul>
+									<li><a href="{{ url('search/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="search">検索</a></li>
+								</ul>
+							</li>
+							@if( isset($global->cce) && (is_object($global->cce) || is_array($global->cce)) )
+								<li><a href="javascript:;">拡張機能</a>
+									<ul>
+										@foreach($global->cce as $cce_id=>$cce_info)
+										<li><a href="{{ url('custom_console_extensions/'.urlencode($cce_id).'/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="custom_console_extensions.{{ $cce_id }}">{{ $cce_info->label }}</a></li>
+										@endforeach
+									</ul>
+								</li>
+							@endif
+							<li><a href="{{ url('staging/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="staging">ステージング管理</a></li>
+							<li><a href="{{ url('delivery/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="delivery">配信管理</a></li>
+							<li><a href="{{ url('clearcache/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="clearcache">キャッシュを消去する</a></li>
+						@endif
+						@if( $global->project_status->pathExists )
+							<li><a href="{{ url('files-and-folders/'.urlencode($project->project_code).'/'.urlencode($branch_name).'/') }}" data-name="files-and-folders">ファイルとフォルダ</a></li>
+						@endif
 					@endif
 				<li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
 					<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
